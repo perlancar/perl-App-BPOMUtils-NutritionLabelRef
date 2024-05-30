5d05fbe5f982adfd21898beeb6d079ad538c187d
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-05-30 15:25:27 +0700

    Release v0.001

---
+++ lib/App/ALG.pm
@@ -0,0 +1,17 @@
+package App::ALG;
+
+use strict;
+
+1;
+# ABSTRACT: Shorter alias for App::BPOMUtils::NutritionLabelRef
+
+=head1 DESCRIPTION
+
+Keywords: BPOM, acuan label gizi
+
+
+=head1 SEE ALSO
+
+L<App::BPOMUtils::NutritionLabelRef>
+
+=cut
